a0e1e561c30e916868b9a857c35b1194dab0426a
Jim MacArthur <jim.macarthur@codethink.co.uk>
2015-09-14 18:02:32 +0100

    Allow RECORD statement with slash-quoted names

--- gcc/fortran/decl.c
+++ gcc/fortran/decl.c
@@ -2581,6 +2581,8 @@ gfc_match_decl_type_spec (gfc_typespec *ts, int implicit_flag)
   match m;
   char c;
   bool seen_deferred_kind, matched_type;
+  char closed_type_string[3];
+  char closing_character;
   const char *dt_name;
 
   /* A belt and braces check that the typespec is correctly being treated
@@ -2614,14 +2616,27 @@ gfc_match_decl_type_spec (gfc_typespec *ts, int implicit_flag)
     }
 
 
+  closing_character = ')';
   m = gfc_match (" type (");
+  if(m != MATCH_YES)
+    {
+      m = gfc_match (" record (");
+      if(m != MATCH_YES)
+	{
+	  m = gfc_match (" record /");
+	  if(m == MATCH_YES)
+	    closing_character = '/';
+	}
+    }
+
   matched_type = (m == MATCH_YES);
   if (matched_type)
     {
       gfc_gobble_whitespace ();
       if (gfc_peek_ascii_char () == '*')
 	{
-	  if ((m = gfc_match ("*)")) != MATCH_YES)
+	  snprintf(closed_type_string, 3, "*%c", closing_character);
+	  if ((m = gfc_match (closed_type_string)) != MATCH_YES)
 	    return m;
 	  if (gfc_current_state () == COMP_DERIVED)
 	    {
@@ -2661,7 +2676,7 @@ gfc_match_decl_type_spec (gfc_typespec *ts, int implicit_flag)
       else
 	m = MATCH_YES;
 
-      if (matched_type && m == MATCH_YES && gfc_match_char (')') != MATCH_YES)
+      if (matched_type && m == MATCH_YES && gfc_match_char (closing_character) != MATCH_YES)
 	m = MATCH_ERROR;
 
       return m;
@@ -2685,7 +2700,7 @@ gfc_match_decl_type_spec (gfc_typespec *ts, int implicit_flag)
 	  && gfc_notify_std (GFC_STD_F2008, "TYPE with "
 			  "intrinsic-type-spec at %C") == FAILURE)
 	return MATCH_ERROR;
-      if (matched_type && gfc_match_char (')') != MATCH_YES)
+      if (matched_type && gfc_match_char (closing_character) != MATCH_YES)
 	return MATCH_ERROR;
 
       ts->type = BT_REAL;
@@ -2716,7 +2731,7 @@ gfc_match_decl_type_spec (gfc_typespec *ts, int implicit_flag)
 			  "intrinsic-type-spec at %C") == FAILURE)
 	return MATCH_ERROR;
 
-      if (matched_type && gfc_match_char (')') != MATCH_YES)
+      if (matched_type && gfc_match_char (closing_character) != MATCH_YES)
 	return MATCH_ERROR;
 
       ts->type = BT_COMPLEX;
@@ -2733,7 +2748,7 @@ gfc_match_decl_type_spec (gfc_typespec *ts, int implicit_flag)
     }
 
   if (matched_type)
-    m = gfc_match_char (')');
+    m = gfc_match_char (closing_character);
 
   if (m == MATCH_YES)
     ts->type = BT_DERIVED;
