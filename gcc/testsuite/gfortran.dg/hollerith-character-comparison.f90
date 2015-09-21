e4d2ce4eb15959c67b6eba1f95926ea6532eb9cc
Jim MacArthur <jim.macarthur@codethink.co.uk>
2015-09-21 14:24:12 +0100

    Add all warning messages to test cases

--- gcc/testsuite/gfortran.dg/hollerith-character-comparison.f90
+++ gcc/testsuite/gfortran.dg/hollerith-character-comparison.f90
@@ -4,11 +4,12 @@
       REAL*4 a
       INTEGER*4 b
       b = 1000
-      print *, 4HJMAC.eq.4HJMAC
-      print *, 4HJMAC.eq."JMAC"
-      print *, 4HJMAC.eq."JMAN"
-      print *, "JMAC".eq.4HJMAN
-      print *, "AAAA".eq.5HAAAAA
-      print *, "BBBBB".eq.5HBBBB
+      print *, 4HJMAC.eq.4HJMAC ! { dg-warning "Legacy Extension: Hollerith constant|Conversion from HOLLERITH" }
+      print *, 4HJMAC.eq."JMAC" ! { dg-warning "Legacy Extension: Hollerith constant|Conversion from HOLLERITH" }
+      print *, 4HJMAC.eq."JMAN" ! { dg-warning "Legacy Extension: Hollerith constant|Conversion from HOLLERITH" }
+      print *, "JMAC".eq.4HJMAN !  { dg-warning "Legacy Extension: Hollerith constant|Conversion from HOLLERITH" }
+      print *, "AAAA".eq.5HAAAAA ! { dg-warning "Legacy Extension: Hollerith constant|Conversion from HOLLERITH" }
+      print *, "BBBBB".eq.5HBBBB ! { dg-warning "Legacy Extension: Hollerith constant|Conversion from HOLLERITH" }
+
       end program
 
