8094ca828008ebaa661a0f9ad4586521f1cf26cf
Jim MacArthur <jim.macarthur@codethink.co.uk>
2015-10-02 18:42:59 +0100

    Very hacky implementation of default widths for floats. Does not take account of variable type (i.e. no COMPLEX).

--- gcc/fortran/io.c
+++ gcc/fortran/io.c
@@ -850,6 +850,13 @@ data_desc:
 
       if (u != FMT_POSINT)
 	{
+	  if (gfc_option.allow_std & GFC_STD_EXTRA_LEGACY)
+	    {
+	      /* Assume a default width based on the variable size.  */
+	      saved_token = u;
+	      break;
+	    }
+
 	  format_locus.nextc += format_string_pos;
 	  gfc_error ("Positive width required in format "
 			 "specifier %s at %L", token_to_string (t),
@@ -924,6 +931,11 @@ data_desc:
 	goto fail;
       if (t != FMT_ZERO && t != FMT_POSINT)
 	{
+	  if (gfc_option.allow_std & GFC_STD_EXTRA_LEGACY)
+	    {
+	      saved_token = t;
+	      break;
+	    }
 	  error = nonneg_required;
 	  goto syntax;
 	}
