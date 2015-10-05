504b9628cd3a82c8d714d80edab98cfce7b585cd
Jim MacArthur <jim.macarthur@codethink.co.uk>
2015-10-05 14:05:03 +0100

    Allow non-integer substring indexes

--- gcc/fortran/resolve.c
+++ gcc/fortran/resolve.c
@@ -4525,6 +4525,17 @@ resolve_substring (gfc_ref *ref)
       if (!gfc_resolve_expr (ref->u.ss.start))
 	return false;
 
+      /* In legacy mode, allow non-integer string indexes by converting */
+      if (ref->u.ss.start->ts.type != BT_INTEGER &&
+	  gfc_numeric_ts (&ref->u.ss.start->ts) &&
+	  gfc_option.allow_std & GFC_STD_EXTRA_LEGACY)
+	{
+	  gfc_typespec t;
+	  t.type = BT_INTEGER;
+	  t.kind = ref->u.ss.start->ts.kind;
+	  gfc_convert_type_warn (ref->u.ss.start, &t, 2, 1);
+	}
+
       if (ref->u.ss.start->ts.type != BT_INTEGER)
 	{
 	  gfc_error ("Substring start index at %L must be of type INTEGER",
@@ -4553,6 +4564,17 @@ resolve_substring (gfc_ref *ref)
     {
       if (!gfc_resolve_expr (ref->u.ss.end))
 	return false;
+
+      /* Non-integer string index endings, as for start */
+      if (ref->u.ss.end->ts.type != BT_INTEGER &&
+	  gfc_numeric_ts (&ref->u.ss.end->ts) &&
+	  gfc_option.allow_std & GFC_STD_EXTRA_LEGACY)
+	{
+	  gfc_typespec t;
+	  t.type = BT_INTEGER;
+	  t.kind = ref->u.ss.end->ts.kind;
+	  gfc_convert_type_warn (ref->u.ss.end, &t, 2, 1);
+	}
 
       if (ref->u.ss.end->ts.type != BT_INTEGER)
 	{
