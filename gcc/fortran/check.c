9987cbc2e42a212b71081cbe6ba04c6b2384dbed
Jim MacArthur <jim.macarthur@codethink.co.uk>
2015-10-05 13:45:15 +0100

    Allow more than one character as argument to ICHAR

--- gcc/fortran/check.c
+++ gcc/fortran/check.c
@@ -1918,7 +1918,7 @@ gfc_check_ichar_iachar (gfc_expr *c, gfc_expr *kind)
   else
     return SUCCESS;
 
-  if (i != 1)
+  if (i != 1 && !gfc_option.allow_std & GFC_STD_EXTRA_LEGACY)
     {
       gfc_error ("Argument of %s at %L must be of length one",
 		 gfc_current_intrinsic, &c->where);
