8143fbdd87f55559dd1ac3eb8e475a7d56b7504a
Jim MacArthur <jim.macarthur@codethink.co.uk>
2015-10-07 18:23:31 -0400

    Allow character-to-int conversions in DATA statements
    This feature is enabled by the `-std=extra-legacy` compiler flag.
    
    Test written by: Francisco Redondo Marchena <francisco.marchena@codethink.co.uk>
    
    Enable this feature with -fdec-char-data-as-int. Also enabled by -fdec.
    The use of -std=extra-legacy has been deprecated.

--- gcc/fortran/options.c
+++ gcc/fortran/options.c
@@ -70,6 +70,7 @@ set_dec_flags (int value)
   flag_dec_math |= value;
   flag_dec_duplicates |= value;
   flag_dec_pad_with_spaces |= value;
+  flag_dec_char_data_as_int |= value;
   flag_dec_hollerith_conversion |= value;
   flag_dec_blank_format_item |= value;
   flag_dec_non_integer_index |= value;
@@ -763,6 +764,7 @@ gfc_handle_option (size_t scode, const char *arg, int value,
       break;
 
     case OPT_std_legacy:
+    case OPT_fdec_char_data_as_int:
     case OPT_fdec_duplicates:
     case OPT_fdec_hollerith_conversion:
     case OPT_flogical_as_integer:
