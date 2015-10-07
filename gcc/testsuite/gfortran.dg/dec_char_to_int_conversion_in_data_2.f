f864585843fe78e2568d533d6fc905b372489fc3
Jim MacArthur <jim.macarthur@codethink.co.uk>
2015-10-07 18:23:31 -0400

    Allow character-to-int conversions in DATA statements
    Use -fdec-char-data-as-int to enable. Also enabled by -fdec.

---
+++ gcc/testsuite/gfortran.dg/dec_char_to_int_conversion_in_data_2.f
@@ -0,0 +1,14 @@
+! { dg-do run }
+! { dg-options "-std=legacy -fdec-char-data-as-int" }
+!
+! Test character to int conversion in DATA types
+!
+! Test case contributed by Jim MacArthur <jim.macarthur@codethink.co.uk>
+! Modified by Mark Eggleston <mark.eggleston@codethink.com>
+!
+        PROGRAM char_int_data_type
+          INTEGER*1 ai(2)
+
+          DATA ai/'1',1/
+          if(ai(1).NE.49) STOP 1
+        END
---
+++ gcc/testsuite/gfortran.dg/dec_char_to_int_conversion_in_data_3.f
@@ -0,0 +1,14 @@
+! { dg-do compile }
+! { dg-options "-std=legacy -fdec -fno-dec-char-data-as-int" }
+!
+! Test character to int conversion in DATA types
+!
+! Test case contributed by Jim MacArthur <jim.macarthur@codethink.co.uk>
+! Modified by Mark Eggleston <mark.eggleston@codethink.com>
+!
+        PROGRAM char_int_data_type
+          INTEGER*1 ai(2)
+
+          DATA ai/'1',1/ ! { dg-error "Incompatible types in DATA" }
+          if(ai(1).NE.49) STOP 1
+        END
