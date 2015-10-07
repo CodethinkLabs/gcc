9127316409342f2bebfb1f3fa34a1df656ce29d3
Jim MacArthur <jim.macarthur@codethink.co.uk>
2015-10-07 16:31:18 -0400

    Convert LOGICAL to INTEGER for arithmetic ops, and vice versa
    We allow converting LOGICAL types to INTEGER when doing arithmetic
    operations, and converting INTEGER types to LOGICAL for use in
    boolean operations.
    
    This feature is enabled with the `-flogical-as-integer` flag. It is also
    enabled with `-std=extra-legacy` which is deprecated.
    
    Note: using this feature will disable bitwise logical operations enabled by
    `-fdec`.

--- gcc/fortran/resolve.c
+++ gcc/fortran/resolve.c
@@ -3627,6 +3627,22 @@ is_character_based (bt type)
    for the conversion.  */
 
 static void
+convert_integer_to_logical (gfc_expr *e)
+{
+  if (e->ts.type == BT_INTEGER)
+    {
+      /* Convert to LOGICAL */
+      gfc_typespec t;
+      t.type = BT_LOGICAL;
+      t.kind = 1;
+      gfc_convert_type_warn (e, &t, 2, 1);
+    }
+}
+
+/* If E is a logical, convert it to an integer and issue a warning
+   for the conversion.  */
+
+static void
 convert_logical_to_integer (gfc_expr *e)
 {
   if (e->ts.type == BT_LOGICAL)
@@ -3770,6 +3786,13 @@ resolve_operator (gfc_expr *e)
     case INTRINSIC_OR:
     case INTRINSIC_EQV:
     case INTRINSIC_NEQV:
+      if (gfc_option.allow_std & GFC_STD_EXTRA_LEGACY 
+	  || flag_logical_as_integer)
+	{
+	  convert_integer_to_logical (op1);
+	  convert_integer_to_logical (op2);
+	}
+
       if (op1->ts.type == BT_LOGICAL && op2->ts.type == BT_LOGICAL)
 	{
 	  e->ts.type = BT_LOGICAL;
@@ -3811,6 +3834,12 @@ resolve_operator (gfc_expr *e)
 	  return resolve_function (e);
 	}
 
+      if (gfc_option.allow_std & GFC_STD_EXTRA_LEGACY
+	  || flag_logical_as_integer)
+	{
+	  convert_integer_to_logical (op1);
+	}
+
       if (op1->ts.type == BT_LOGICAL)
 	{
 	  e->ts.type = BT_LOGICAL;
@@ -3843,7 +3872,8 @@ resolve_operator (gfc_expr *e)
     case INTRINSIC_NE:
     case INTRINSIC_NE_OS:
 
-      if (gfc_option.allow_std & GFC_STD_EXTRA_LEGACY)
+      if (gfc_option.allow_std & GFC_STD_EXTRA_LEGACY
+          || flag_logical_as_integer)
 	{
 	  convert_logical_to_integer (op1);
 	  convert_logical_to_integer (op2);
