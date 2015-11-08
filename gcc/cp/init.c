CodethinkLabs/gcc

commit 8a4e999422f248a593ffafe87c5364f1ee25f31c

PR c++/67942 - diagnose placement new buffer overflow
gcc/cp/
	* cp/init.c (warn_placement_new_too_small): Avoid assuming
	the size of the first operand of placement new or its type
	is known.

gcc/testsuite/
	* g++.dg/warn/Wplacement-new-size.C: Exercise placement new
	invocations where the size of the destination buffer object
	or its type (or both) is unknown.


git-svn-id: svn://gcc.gnu.org/svn/gcc/trunk@229831 138bc75d-0d04-0410-961f-82ee72b054a4

gcc/cp/init.c
@@ -2384,19 +2384,25 @@ warn_placement_new_too_small (tree type, tree nelts, tree size, tree oper)
       /* Treat members of unions and members of structs uniformly, even
 	 though the size of a member of a union may be viewed as extending
 	 to the end of the union itself (it is by __builtin_object_size).  */
-      if (TREE_CODE (oper) == VAR_DECL || use_obj_size)
+      if ((TREE_CODE (oper) == VAR_DECL || use_obj_size)
+	  && DECL_SIZE_UNIT (oper))
 	{
 	  /* Use the size of the entire array object when the expression
 	     refers to a variable or its size depends on an expression
 	     that's not a compile-time constant.  */
-	  bytes_avail = tree_to_shwi (DECL_SIZE_UNIT (oper));
+	  bytes_avail = tree_to_uhwi (DECL_SIZE_UNIT (oper));
 	  exact_size = !use_obj_size;
 	}
-      else
+      else if (TYPE_SIZE_UNIT (TREE_TYPE (oper)))
 	{
 	  /* Use the size of the type of the destination buffer object
 	     as the optimistic estimate of the available space in it.  */
 	  bytes_avail = tree_to_uhwi (TYPE_SIZE_UNIT (TREE_TYPE (oper)));
+	}
+      else
+	{
+	  /* Bail if neither the size of the object nor its type is known.  */
+	  return;
 	}
 
       /* Avoid diagnosing flexible array members (accepted as an extension

gcc/testsuite/g++.dg/warn/Wplacement-new-size.C
@@ -408,3 +408,48 @@ void test_user_defined_placement_new ()
         new (&x) ClassWithGlobalNew[2];
     }
 }
+
+extern char extbuf[];
+
+template <class> struct TemplateClass { char c; };
+
+// Declare a specialization but don't provide a definition.
+template <> struct TemplateClass<void>;
+
+// Declare an object of an explicit specialization of an unknown size.
+extern TemplateClass<void> exttempl_void;
+
+// Verify that no warning is issued when placement new is called with
+// an extern buffer of unknown size (and the case is handled gracefully
+// and doesn't cause an ICE).
+static __attribute__ ((used))
+void test_extern_buffer_of_unknown_size ()
+{
+    new (extbuf) int ();
+    new (extbuf) int [1024];
+
+    new (&exttempl_void) int ();
+    new (&exttempl_void) int [1024];
+}
+
+extern char extbuf_size_int [sizeof (int)];
+
+extern TemplateClass<int> exttempl;
+
+// Verify that a warning is issued as expected when placement new is
+// called with an extern buffer of known size (and the case is handled
+// gracefully and doesn't cause an ICE).
+static __attribute__ ((used))
+void test_extern_buffer ()
+{
+    new (extbuf_size_int) int ();
+    new (extbuf_size_int) int [1];
+
+    struct S { int a [2]; };
+
+    new (extbuf_size_int) S;            // { dg-warning "placement" }
+    new (extbuf_size_int) int [2];      // { dg-warning "placement" }
+
+    new (&exttempl) int ();             // { dg-warning "placement" }
+    new (&exttempl) int [1024];         // { dg-warning "placement" }
+}

commit 29bd1f9516c2dce769acd33ee833ae349710b00e

Clean up fallout on ILP32 from r229831.
gcc/
	PR c++/67942 
	* cp/init.c (warn_placement_new_too_small): Convert integer
	operand of POINTER_PLUS_EXPR to ssize_t to determine its signed
	value.

c-family/
        * c.opt (Wplacement-new): Add a period to the end of
        a sentence.


git-svn-id: svn://gcc.gnu.org/svn/gcc/trunk@229959 138bc75d-0d04-0410-961f-82ee72b054a4

gcc/c-family/c.opt
@@ -778,7 +778,7 @@ Warn if inherited methods are unimplemented.
 
 Wplacement-new
 C++ Var(warn_placement_new) Init(1) Warning
-Warn for placement new expressions with undefined behavior
+Warn for placement new expressions with undefined behavior.
 
 Wredundant-decls
 C ObjC C++ ObjC++ Var(warn_redundant_decls) Warning

gcc/cp/init.c
@@ -2321,12 +2321,14 @@ warn_placement_new_too_small (tree type, tree nelts, tree size, tree oper)
   if (TREE_CODE (oper) == POINTER_PLUS_EXPR)
     {
       /* If the offset is comple-time constant, use it to compute a more
-	 accurate estimate of the size of the buffer.  Otherwise, use
-	 the size of the entire array as an optimistic estimate (this
-	 may lead to false negatives).  */
-      const_tree adj = TREE_OPERAND (oper, 1);
+	 accurate estimate of the size of the buffer.  Since the operand
+	 of POINTER_PLUS_EXPR is represented as an unsigned type, convert
+	 it to signed first.
+	 Otherwise, use the size of the entire array as an optimistic
+	 estimate (this may lead to false negatives).  */
+      tree adj = TREE_OPERAND (oper, 1);
       if (CONSTANT_CLASS_P (adj))
-	adjust += tree_to_uhwi (adj);
+	adjust += tree_to_shwi (convert (ssizetype, adj));
       else
 	use_obj_size = true;
 