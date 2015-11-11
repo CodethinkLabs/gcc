2d360a4cd606be51b4ab9e6fbf2df947e7a169ae
Jim MacArthur <jim.macarthur@codethink.co.uk>
2015-11-11 18:50:19 +0000

    Allow redefinition of types for procedures

--- gcc/fortran/symbol.c
+++ gcc/fortran/symbol.c
@@ -1761,7 +1761,7 @@ gfc_add_type (gfc_symbol *sym, gfc_typespec *ts, locus *where)
 	    /* Ignore temporaries and class/procedure names */
 	    return false;
 	  }
-	  if (gfc_compare_types(&sym->ts, ts) && (flavor == FL_UNKNOWN || flavor == FL_VARIABLE))
+	  if (gfc_compare_types(&sym->ts, ts) && (flavor == FL_UNKNOWN || flavor == FL_VARIABLE || flavor == FL_PROCEDURE))
 	    {
 	      return gfc_notify_std (GFC_STD_LEGACY,
 				     "Symbol '%s' at %L already has basic type of %s", sym->name,
