2eb6794cea988d304d8a5bbbb0ec640703378c95
Jim MacArthur <jim.macarthur@codethink.co.uk>
2015-11-11 15:37:00 +0000

    Allow non-logical expressions in IF statements
    This feature is enabled by the `-std=extra-legacy` compiler flag.
    
    Signed-off-by: Ben Brewer <ben.brewer@codethink.co.uk>
    Signed-off-by: Francisco Redondo Marchena <francisco.marchena@codethink.co.uk>
    
    Tests written by: Francisco Redondo Marchena <francisco.marchena@codethink.co.uk>
                      Jeff Law <law@redhat.com>

---
+++ gcc/testsuite/gfortran.dg/dec_logical_expressions_if_statements_blocks-2.f
@@ -0,0 +1,23 @@
+! { dg-do compile }
+! { dg-options "-std=extra-legacy" }
+
+       function othersub1()
+        integer*4 othersub1
+        othersub1 = 1
+       end
+       function othersub2()
+        integer*4 othersub2
+        othersub2 = 2
+       end
+       program MAIN
+        integer*4 othersub1
+        integer*4 othersub2
+c the if (integer) works here 
+        if (othersub2()) then		! { dg-warning "" }
+         write (*,*), 'othersub2 is true'
+c but fails in the "else if"
+        else if (othersub1()) then	! { dg-warning "" }
+         write (*,*), 'othersub2 is false, othersub1 is true'
+        endif
+       end
+
