4ecac268aa58a0ef16cfcc4b73b455e111d2c63a
Jim MacArthur <jim.macarthur@codethink.co.uk>
2015-11-17 15:05:02 +0000

    Allow character-to-int comparisons.

--- gcc/fortran/resolve.c
+++ gcc/fortran/resolve.c
@@ -4103,19 +4103,19 @@ resolve_operator (gfc_expr *e)
 	  break;
 	}
 
-
-      if(gfc_option.flag_oracle_support && gfc_numeric_ts(&op1->ts) && op2->ts.type == BT_HOLLERITH)
+      /* Numeric to hollerith comparisons */
+      if(gfc_option.flag_oracle_support && gfc_numeric_ts(&op1->ts) && (op2->ts.type == BT_HOLLERITH || op2->ts.type == BT_CHARACTER))
 	{
-	  gfc_warning("Promoting argument for comparison from HOLLERITH to INTEGER at %L", &op2->where);
+	  gfc_warning("Promoting argument for comparison from character type to INTEGER at %L", &op2->where);
 	  gfc_typespec ts;
 	  ts.type = BT_INTEGER;
 	  ts.kind = 4;
 	  gfc_convert_type_warn (op2, &ts, 2, 1);
 	}
 
-      if(gfc_option.flag_oracle_support && gfc_numeric_ts(&op2->ts) && op1->ts.type == BT_HOLLERITH)
+      if(gfc_option.flag_oracle_support && gfc_numeric_ts(&op2->ts) && (op1->ts.type == BT_HOLLERITH || op1->ts.type == BT_CHARACTER))
 	{
-	  gfc_warning("Promoting argument for comparison from HOLLERITH to INTEGER at %L", &op1->where);
+	  gfc_warning("Promoting argument for comparison from character type to INTEGER at %L", &op1->where);
 	  gfc_typespec ts;
 	  ts.type = BT_INTEGER;
 	  ts.kind = 4;
