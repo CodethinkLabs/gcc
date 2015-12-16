d10c501c9baca7665753d1f85634d25d351fa9c9
Jim MacArthur <jim.macarthur@codethink.co.uk>
2015-12-16 17:00:19 +0000

    Correct warnings expected in hollerith-int-comparison test

--- gcc/testsuite/gfortran.dg/hollerith-int-comparison.f90
+++ gcc/testsuite/gfortran.dg/hollerith-int-comparison.f90
@@ -2,10 +2,10 @@
 
       program convert
       INTEGER*4 b
-      b = 5HRIVET
-      print *, 4HJMAC.eq.400 ! { dg-warning "Promoting argument for comparison from HOLLERITH" }
-      print *, 4HRIVE.eq.1163282770 ! { dg-warning "Promoting argument for comparison from HOLLERITH" }
+      b = 5HRIVET ! { dg-warning "Legacy Extension: Hollerith constant|Conversion from HOLLERITH to INTEGER|too long to convert" }
+      print *, 4HJMAC.eq.400 ! { dg-warning "Legacy Extension: Hollerith constant|Promoting argument for comparison from character|Conversion from HOLLERITH to INTEGER" }
+      print *, 4HRIVE.eq.1163282770 ! { dg-warning "Legacy Extension: Hollerith constant|Promoting argument for comparison from character|Conversion from HOLLERITH to INTEGER" }
       print *, b
-      print *, 1163282770.eq.4HRIVE ! { dg-warning "Promoting argument for comparison from HOLLERITH" }
+      print *, 1163282770.eq.4HRIVE ! { dg-warning "Legacy Extension: Hollerith constant|Promoting argument for comparison from character|Conversion from HOLLERITH to INTEGER" }
       end program
 
