56bf5e76d86385fb6799ad4d5afb351fe85fb688
Jim MacArthur <jim.macarthur@codethink.co.uk>
2016-02-04 17:18:30 +0000

    Allow conversion between Hollerith constants and CHARACTER and INTEGER
    Warnings are raised when this happens.
    
    This feature is enabled with the `-std=extra-legacy` compiler flag.
    
            0004-Allow-conversion-between-Hollerith-constants-and-CHA.patch
    
    Signed-off-by: Francisco Redondo Marchena <francisco.marchena@codethink.co.uk>
    
    Add -fdec-hollerith-conversion
    
    Enable automatic conversion from Hollerith constants to character and
    integer types. Also enabled by -fdec. The use of -std=extra-legacy
    for this feature has been deprecated.

---
+++ gcc/testsuite/gfortran.dg/hollerith-int-comparison.f90
@@ -0,0 +1,28 @@
+      ! { dg-do run }
+      ! { dg-options "-std=extra-legacy" }
+
+      program convert
+      integer*4 b
+      integer*4 abcd
+      logical bigendian
+      abcd = ichar ("a")
+      abcd = 256 * abcd + ichar ("b")
+      abcd = 256 * abcd + ichar ("c")
+      abcd = 256 * abcd + ichar ("d")
+      bigendian = transfer (abcd, "wxyz") .eq. "abcd"
+      ! Note: 5HRIVET will be truncated to "RIVE" and then converted to integer
+      b = 5HRIVET ! { dg-warning "too long to convert" }
+      if (4HJMAC.eq.400) stop 1 ! { dg-warning "Promoting argument for comparison from character" }
+      if (bigendian) then
+        if (4HRIVE.ne.1380537925) stop 2 ! { dg-warning "Promoting argument for comparison from character" }
+      else
+        if (4HRIVE.ne.1163282770) stop 2 ! { dg-warning "Promoting argument for comparison from character" }
+      end if
+      if (b.ne.4HRIVE) stop 3            ! { dg-warning "Promoting argument for comparison from character" }
+      if (bigendian) then
+        if (1380537925.ne.4HRIVE) stop 4 ! { dg-warning "Promoting argument for comparison from character" }
+      else
+        if (1163282770.ne.4HRIVE) stop 4 ! { dg-warning "Promoting argument for comparison from character" }
+      end if
+      end program
+
