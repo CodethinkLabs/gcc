79a5a0b65773efc5b83a745695e2686d24d30933
Jim MacArthur <jim.macarthur@codethink.co.uk>
2016-02-04 16:59:41 +0000

    Allow blank format items in format strings
    This has to be written in a slightly verbose manner because GCC 7
    defaults to building with -Werror=implicit-fallthrough which prevents
    us from just falling through to the default: case.
    
    This feature is enabled by the `-std=extra-legacy` compiler flag.
    
    Test written by: Francisco Redondo Marchena <francisco.marchena@codethink.co.uk>
    
    Add -fdec-blank-format-item
    
    Enable blank format items in format strings. Also enabled by -fdec.
    Replaces use of -std=extra-legacy which is deprecated.
    
    Tests added for -fdec and -fdec-blank-format-item. { dg-do run } used
    to execute the test programs.

--- gcc/fortran/options.c
+++ gcc/fortran/options.c
@@ -77,6 +77,7 @@ set_dec_flags (int value)
   SET_BITFLAG (flag_dec_duplicates, value, value);
   SET_BITFLAG (flag_dec_pad_with_spaces, value, value);
   SET_BITFLAG (flag_dec_hollerith_conversion, value, value);
+  SET_BITFLAG (flag_dec_blank_format_item, value, value);
 }
 
 /* Finalize DEC flags.  */
