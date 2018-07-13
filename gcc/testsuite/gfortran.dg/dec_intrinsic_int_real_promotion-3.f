350c52e6df48aaa8d090d48e741d306cc1b2aa27
Jeff Law <law@redhat.com>
2018-07-13 10:30:12 -0600

    Update uses of variables to use new names

--- gcc/testsuite/gfortran.dg/dec_intrinsic_int_real_promotion-3.f
+++ gcc/testsuite/gfortran.dg/dec_intrinsic_int_real_promotion-3.f
@@ -38,77 +38,77 @@
         INTEGER mx_i/0/
         REAL mx_r/0.0/
 
-        m_i = MOD(a_i, b_i)                     ! { dg-error "" }
+        m_i = MOD(a_i, b_c)                     ! { dg-error "" }
         if (m_i .ne. 1) STOP 1
-        m_i = MOD(a2_i, b2_i)                   ! { dg-error "" }
+        m_i = MOD(a2_i, b2_c)                   ! { dg-error "" }
         if (m_i .ne. 1) STOP 2
-        m_r = MOD(a_r, b_r)                     ! { dg-error "" }
+        m_r = MOD(a_r, b_l)                     ! { dg-error "" }
         if (abs(m_r - 1.0) > 1.0D-6) STOP 3
-        m_r = MOD(a2_r, b2_r)                   ! { dg-error "" }
+        m_r = MOD(a2_r, b2_l)                   ! { dg-error "" }
         if (abs(m_r - 1.0) > 1.0D-6) STOP 4
-        m_r = MOD(a_i, b_r)                     ! { dg-error "" }
+        m_r = MOD(a_i, b_l)                     ! { dg-error "" }
         if (abs(m_r - 1.0) > 1.0D-6) STOP 5
-        m_r = MOD(a_r, b_i)                     ! { dg-error "" }
+        m_r = MOD(a_r, b_c)                     ! { dg-error "" }
         if (abs(m_r - 1.0) > 1.0D-6) STOP 6
 
-        md_i = MODULO(a_i, b_i)                 ! { dg-error "" }
+        md_i = MODULO(a_i, b_c)                 ! { dg-error "" }
         if (md_i .ne. 1) STOP 7
-        md_i = MODULO(a2_i, b2_i)               ! { dg-error "" }
+        md_i = MODULO(a2_i, b2_c)               ! { dg-error "" }
         if (md_i .ne. 1) STOP 8
-        md_r = MODULO(a_r, b_r)                 ! { dg-error "" }
+        md_r = MODULO(a_r, b_l)                 ! { dg-error "" }
         if (abs(md_r - 1.0) > 1.0D-6) STOP 9
-        md_r = MODULO(a2_r, b2_r)               ! { dg-error "" }
+        md_r = MODULO(a2_r, b2_l)               ! { dg-error "" }
         if (abs(md_r - 1.0) > 1.0D-6) STOP 10
-        md_r = MODULO(a_i, b_r)                 ! { dg-error "" }
+        md_r = MODULO(a_i, b_l)                 ! { dg-error "" }
         if (abs(md_r - 1.0) > 1.0D-6) STOP 11
-        md_r = MODULO(a_r, b_i)                 ! { dg-error "" }
+        md_r = MODULO(a_r, b_c)                 ! { dg-error "" }
         if (abs(md_r - 1.0) > 1.0D-6) STOP 12
 
-        d_i = DIM(a_i, b_i)                     ! { dg-error "" }
+        d_i = DIM(a_i, b_c)                     ! { dg-error "" }
         if (d_i .ne. 1) STOP 13
-        d_i = DIM(a2_i, b2_i)                   ! { dg-error "" }
+        d_i = DIM(a2_i, b2_c)                   ! { dg-error "" }
         if (d_i .ne. 1) STOP 14
-        d_r = DIM(a_r, b_r)                     ! { dg-error "" }
+        d_r = DIM(a_r, b_l)                     ! { dg-error "" }
         if (abs(d_r - 1.0) > 1.0D-6) STOP 15
-        d_r = DIM(a2_r, b2_r)                   ! { dg-error "" }
+        d_r = DIM(a2_r, b2_l)                   ! { dg-error "" }
         if (abs(d_r - 1.0) > 1.0D-6) STOP 16
-        d_r = DIM(a_r, b_i)                     ! { dg-error "" }
+        d_r = DIM(a_r, b_c)                     ! { dg-error "" }
         if (abs(d_r - 1.0) > 1.0D-6) STOP 17
-        d_r = DIM(b_i, a_r)                     ! { dg-error "" }
+        d_r = DIM(b_c, a_r)                     ! { dg-error "" }
         if (abs(d_r) > 1.0D-6) STOP 18
 
-        s_i = SIGN(-a_i, b_i)                   ! { dg-error "" }
+        s_i = SIGN(-a_i, b_c)                   ! { dg-error "" }
         if (s_i .ne. 4) STOP 19
-        s_i = SIGN(-a2_i, b2_i)                 ! { dg-error "" }
+        s_i = SIGN(-a2_i, b2_c)                 ! { dg-error "" }
         if (s_i .ne. 4) STOP 20
-        s_r = SIGN(a_r, -b_r)                   ! { dg-error "" }
+        s_r = SIGN(a_r, -b_l)                   ! { dg-error "" }
         if (abs(s_r - (-a_r)) > 1.0D-6) STOP 21
-        s_r = SIGN(a2_r, -b2_r)                 ! { dg-error "" }
+        s_r = SIGN(a2_r, -b2_l)                 ! { dg-error "" }
         if (abs(s_r - (-a2_r)) > 1.0D-6) STOP 22
-        s_r = SIGN(a_r, -b_i)                   ! { dg-error "" }
+        s_r = SIGN(a_r, -b_c)                   ! { dg-error "" }
         if (abs(s_r - (-a_r)) > 1.0D-6) STOP 23
-        s_r = SIGN(-a_i, b_r)                   ! { dg-error "" }
+        s_r = SIGN(-a_i, b_l)                   ! { dg-error "" }
         if (abs(s_r - a_r) > 1.0D-6) STOP 24
 
-        mx_i = MAX(-a_i, -b_i, x_i, y_i)        ! { dg-error "" }
+        mx_i = MAX(-a_i, -b_c, x_i, y_c)        ! { dg-error "" }
         if (mx_i .ne. x_i) STOP 25
-        mx_i = MAX(-a2_i, -b2_i, x_i, y_i)      ! { dg-error "" }
+        mx_i = MAX(-a2_i, -b2_c, x_i, y_c)      ! { dg-error "" }
         if (mx_i .ne. x_i) STOP 26
-        mx_r = MAX(-a_r, -b_r, x_r, y_r)        ! { dg-error "" }
+        mx_r = MAX(-a_r, -b_l, x_r, y_l)        ! { dg-error "" }
         if (abs(mx_r - x_r) > 1.0D-6) STOP 27
-        mx_r = MAX(-a_r, -b_r, x_r, y_r)        ! { dg-error "" }
+        mx_r = MAX(-a_r, -b_l, x_r, y_l)        ! { dg-error "" }
         if (abs(mx_r - x_r) > 1.0D-6) STOP 28
-        mx_r = MAX(-a_i, -b_r, x_r, y_i)        ! { dg-error "" }
+        mx_r = MAX(-a_i, -b_l, x_r, y_c)        ! { dg-error "" }
         if (abs(mx_r - x_r) > 1.0D-6) STOP 29
 
-        mn_i = MIN(-a_i, -b_i, x_i, y_i)        ! { dg-error "" }
+        mn_i = MIN(-a_i, -b_c, x_i, y_c)        ! { dg-error "" }
         if (mn_i .ne. -a_i) STOP 31
-        mn_i = MIN(-a2_i, -b2_i, x_i, y_i)      ! { dg-error "" }
+        mn_i = MIN(-a2_i, -b2_c, x_i, y_c)      ! { dg-error "" }
         if (mn_i .ne. -a2_i) STOP 32
-        mn_r = MIN(-a_r, -b_r, x_r, y_r)        ! { dg-error "" }
+        mn_r = MIN(-a_r, -b_l, x_r, y_l)        ! { dg-error "" }
         if (abs(mn_r - (-a_r)) > 1.0D-6) STOP 33
-        mn_r = MIN(-a2_r, -b2_r, x_r, y_r)      ! { dg-error "" }
+        mn_r = MIN(-a2_r, -b2_l, x_r, y_l)      ! { dg-error "" }
         if (abs(mn_r - (-a2_r)) > 1.0D-6) STOP 34
-        mn_r = MIN(-a_i, -b_r, x_r, y_i)        ! { dg-error "" }
+        mn_r = MIN(-a_i, -b_l, x_r, y_c)        ! { dg-error "" }
         if (abs(mn_r - (-a_r)) > 1.0D-6) STOP 35
       END PROGRAM
