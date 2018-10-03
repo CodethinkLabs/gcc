8cafaae4eaf9beb474b4914ab3983f468c9ff72b
Mark Eggleston <markeggleston@codethink.com>
2018-10-03 12:00:07 +0100

    Ensure tests are performed for -fdec-non-integer-index

--- gcc/testsuite/gfortran.dg/dec_not_integer_substring_indexes.f
+++ gcc/testsuite/gfortran.dg/dec_not_integer_substring_indexes.f
@@ -5,13 +5,12 @@
 !
         PROGRAM not_integer_substring_indexes
           CHARACTER*5 st/'Tests'/
-          CHARACTER*4 st2
           REAL ir/1.0/
           REAL ir2/4.0/
 
-          st2 = st(ir:4)
-          st2 = st(1:ir2)
-          st2 = st(1.0:4)
-          st2 = st(1:4.0)
-          st2 = st(1.5:4)
+          if (st(ir:4).ne.'Test') stop 1
+          if (st(1:ir2).ne.'Test') stop 2
+          if (st(1.0:4).ne.'Test') stop 3
+          if (st(1:4.0).ne.'Test') stop 4
+          if (st(2.5:4).ne.'est') stop 5
         END
