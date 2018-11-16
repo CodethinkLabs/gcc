0c7e09c8ceed9a9310ca5bc03dc50f5f75a9a8f3
Mark Eggleston <markeggleston@codethink.com>
2018-11-16 13:37:50 +0000

    Revise -fdec-duplicates test cases

--- gcc/testsuite/gfortran.dg/duplicate_type_11.f
+++
@@ -1,9 +0,0 @@
-! { dg-do run }
-! { dg-options "-std=legacy -fdec-duplicates" }
-
-      PROGRAM TEST
-      INTEGER :: X
-      INTEGER :: X
-      X = 42
-      IF (X.NE.42) STOP 1
-      END PROGRAM TEST
